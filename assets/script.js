aa1702f10815fa569c648c2f2769f4896d9dcce8
DyusmikeevEA <rfeugenerf@gmail.com>
2022-12-20 06:51:10 +0500

    first try to finish

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,6 @@ let burger = document.querySelector('.header__row-burgerMenu')
 let menu = document.querySelector('.header__row-menu')
 let catalogBurger = document.querySelector('.catalog__genre-burger')
 let catalogMenu = document.querySelector('.catalog__genre-menu')
-let profileBurger = document.querySelector('.profile-mobile-right')
-let profileMenu = document.querySelector('.profile-mobile-select')
 
 burger.addEventListener("click", ()=>{
     menu.classList.toggle('none')
@@ -11,10 +9,5 @@ burger.addEventListener("click", ()=>{
 
 catalogBurger.addEventListener("click", ()=>{
     catalogMenu.classList.toggle('none')
-    console.log(1)
-})
-
-profileBurger.addEventListener("click", ()=>{
-    profileMenu.classList.toggle('profileNone')
     console.log(1)
 })
---
+++ assets/script2.js
@@ -0,0 +1,6 @@
+let profileBurger = document.querySelector('.profile-mobile-right')
+let profileMenu = document.querySelector('.profile-mobile-select')
+
+profileBurger.addEventListener('click', ()=>{
+    profileMenu.classList.toggle('none')
+})
